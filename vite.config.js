cd165234b0bc3564f633bd2b531bdce4814e9d9a
Hirofumi Kanda <nandakandahirofumi322@yahoo.co.jp>
2025-07-20 17:34:51 +0900

    デプロイパス修正

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH ? `/${process.env.VITE_BASE_PATH}/` : '/',
-  build: {
-    sourcemap: true
-  }
-})
+  base: "/maplibre-wind-current/",
+});

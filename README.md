808f907397424c5b36a9309b05be30e27e8ea3ef
Hirofumi Kanda <nandakandahirofumi322@yahoo.co.jp>
2025-07-20 17:24:14 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Maplibre-Wind-Current
+
+[jma-wind-map](https://github.com/naogify/jma-wind-map)をクローンして作成
+
---
+++ src/main.js
@@ -0,0 +1,43 @@
+import { MapboxOverlay } from '@deck.gl/mapbox';
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './style.css'
+import * as WeatherLayers from 'weatherlayers-gl';
+
+const map = new maplibregl.Map({
+  container: 'map',
+  style: './style/style.json',
+  center: [136.51, 37.88],
+  zoom: 2.8,
+  attributionControl: {
+    customAttribution: '<a href="https://www.data.jma.go.jp/developer/gpv_sample.html" target="_blank">出典：気象庁「全球数値予報モデルGPV (GSM全球域・日本域)」を加工して作成</a>',
+  }
+});
+
+map.on('load', async () => {
+
+  map.setProjection({
+    type: 'globe', // Set projection to globe
+  });
+
+  const image = await WeatherLayers.loadTextureData('./img/wind.png');
+
+  const deckOverlay = new MapboxOverlay({
+    interleaved: true,
+    layers: [
+      new WeatherLayers.ParticleLayer({
+        id: 'particle',
+        numParticles: 5000,
+        maxAge: 10,
+        speedFactor: 30,
+        width: 2.0,
+        opacity: 0.05,
+        image: image,
+        bounds: [-180, -90, 180, 90],
+        imageUnscale: [-128, 127],
+      }),
+    ]
+  });
+
+  map.addControl(deckOverlay);
+});
